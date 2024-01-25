880d3220ca69a2cd3df1480fb13ce7943b7b6925
vidhikabathani <vidhikabathani110@gmail.com>
2024-01-25 17:59:12 +0530

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,8 +47,39 @@ function Home() {
 
             {/* ABOUT SECTION  */}
             <section className='f-white'>
+                <div id="about_content">
+                    <div className="col-5 about-img">
+                        <img src="https://preview.colorlib.com/theme/coffeeblend/images/about.jpg.webp" alt="" />
+                    </div>
+                    <div className="col-5">
+                        <div className="about-overlay">
+                            <h1 className='Greate-vibe f-primary'>Discover</h1>
+                            <h2>OUR STORY</h2>
+                            <p className='f-gray fs-6'>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* SERVICES SECTION  */}
+            <section className='p-100 bg-p'>
                 <div className="container">
-                    vidhika
+
+                </div>
+            </section>
+
+            {/* MENU SECTION  */}
+            <section className='p-100'>
+                <div className="container">
+                    <div id="menu_content">
+                        <div className="col-6 align-end">
+                            <h1 className='f-primary Greate-vibe'>Discover</h1>
+                            <h2 className='f-white'>OUR MENU</h2>
+                            <p className='f-gray'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                            <button className='btn-transparent'>View Full Menu</button>
+                        </div>
+                        <div className="col-6"></div>
+                    </div>
                 </div>
             </section>
         </div>
