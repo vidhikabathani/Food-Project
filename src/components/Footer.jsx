4218580abe3a14c3f8b960b46780a7750671fb85
vidhikabathani <vidhikabathani110@gmail.com>
2024-06-22 11:05:41 +0530

    food

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,49 @@ function Footer(){
             <div className="container">
                 <div className="row">
                     <div className="col-lg-4 col-md-6 col-12">
-                        <h5>CONTACT US </h5>
+                        <div className='pb-5'>
+                        <h5 className='pb-3'>CONTACT US </h5>
                         <span className='f-gray-light fs-6'>8th floor, 379 Hudson St, New York.</span><br />
                         <span className='f-gray-light '>(+1) 96 716 6879</span><br />
                         <span className='f-gray-light '>contact@site.com</span>
+                        </div>
+                        <div>
+                        <h5 className='pb-3'>OPENING TIMES</h5>
+                        <span className='f-gray-light '>09:30 AM – 11:00 PM</span><br />
+                        <span className='f-gray-light '>Every Day</span>
+                        </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-12">
-                        vidhika
+                        <div className='pb-5'>
+                        <h5 className='pb-3'>LATEST TWITTER</h5>
+                        <span className='f-primary pointer'>@patoplace</span><br />
+                        <span className='f-gray-light'>Activello is a good option. It has a slider built into that displays the featured image in the slider.</span><br />
+                        <span className='f-primary pointer'>https://buff.ly/2zaSfAQ</span><br />
+                        <span className='f-gray f-6'>21 Dec 2017</span>
+                        </div>
+                        <span className='f-primary pointer'>@patoplace</span><br />
+                        <span className='f-gray-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+                        <span className='f-primary pointer'> https://buff.ly/2zaSfAQ</span><br />
+                        <span className='f-gray f-6'>15 Feb 2022</span>
                     </div>
                     <div className="col-lg-4 col-md-6 col-12">
-                        vidhika
+                        <div className="pb-5">
+                            <h5 className="pb-3"> GALLERY</h5>
+                            <div className='row'>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-01.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-02.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-03.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-04.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-05.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-06.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-07.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-08.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-09.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-10.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-11.jpg.webp" alt="" /></div>
+                                <div className="col-lg-3 p-r-0 pb-3"> <img src="https://preview.colorlib.com/theme/pato/images/photo-gallery-thumb-12.jpg.webp" alt="" /></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
 
 function Home() {
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
     return (
         <div className='bg-gray'>
 
             {/* BANNER SECTION  */}
-            <section className='banner-bg'>
-                <div className="banner-layout">
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <Image src="https://preview.colorlib.com/theme/pato/images/master-slides-02.jpg.webp"/>
+                    <Carousel.Caption>
                     <div className="container">
                         <div id="banner_content">
                             <span className='Greate-vibe fs-1 f-primary'>Welcome</span>
@@ -16,8 +26,35 @@ function Home() {
                             <button className='btn btn-transparent'>LOOK MENU</button>
                         </div>
                     </div>
-                </div>
-            </section>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Image src="https://preview.colorlib.com/theme/pato/images/master-slides-01.jpg.webp" rounded />
+                    <Carousel.Caption>
+                    <div className="container">
+                        <div id="banner_content">
+                            <span className='Greate-vibe fs-1 f-primary'>Welcome</span>
+                            <h1 className='fs-1 poppins fw-semibold'>PATO PLACE</h1>
+                            <p className='fs-5' id='banner-extra'>A small river named Duden flows by their place and supplies it with the necessary regelialia.  ipsum dolor sit amet, consectetur adipisicing elit. Molestiae assumenda autem sequi labore consequatur quod, river named Duden recusandae place and supplies. </p>
+                            <button className='btn btn-transparent'>LOOK MENU</button>
+                        </div>
+                    </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <Image src="https://preview.colorlib.com/theme/pato/images/master-slides-01.jpg.webp" rounded />
+                    <Carousel.Caption>
+                    <div className="container">
+                        <div id="banner_content">
+                            <span className='Greate-vibe fs-1 f-primary'>Welcome</span>
+                            <h1 className='fs-1 poppins fw-semibold'>PATO PLACE</h1>
+                            <p className='fs-5' id='banner-extra'>A small river named Duden flows by their place and supplies it with the necessary regelialia.  ipsum dolor sit amet, consectetur adipisicing elit. Molestiae assumenda autem sequi labore consequatur quod, river named Duden recusandae place and supplies. </p>
+                            <button className='btn btn-transparent'>LOOK MENU</button>
+                        </div>
+                    </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
 
             {/* CONTACT SECTION  */}
             <section className='py-5 f-white'>
@@ -32,8 +69,8 @@ function Home() {
                             <p className='fs-6 f-gray'>203 Fake St. Mountain View, San Francisco, California, USA</p>
                         </div>
                         <div id="contact" className='col-12 col-md-3 py-2 py-md-0'>
-                            <h5 className='f-gray fw-bold'>Open Monday-Friday</h5>
-                            <p className='fs-6 f-gray'>8:00am - 9:00pm</p>
+                            <h5 className='f-gray fw-bold'>Open Every Day</h5>
+                            <p className='fs-6 f-gray'>09:30 AM – 11:00 PM</p>
                         </div>
                         <div id="contact" className='col-12 col-md-3 py-2 py-md-0'>
                             <p className='fs-6 f-gray'>The countries Vokalia and Consonantia, there live the blind texts.</p>
@@ -46,17 +83,17 @@ function Home() {
             {/* ABOUT SECTION  */}
             <section className='f-white'>
                 <div className="container">
-                <div  className='row align-items-center'>
-                    <div className="col-12 col-md-6 p-5 about-img">
-                        <img src="https://preview.colorlib.com/theme/pato/images/our-story-01.jpg.webp" className='w-75 rounded' alt="" />
-                    </div>
-                    <div className="col-12 col-md-6">
-                        <div>
-                            <h1 className='Greate-vibe f-primary'>Italian Restaurant</h1>
-                            <p className='f-gray fs-6'>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+                    <div className='row align-items-center'>
+                        <div className="col-12 col-md-6 p-5 about-img">
+                            <img src="https://preview.colorlib.com/theme/pato/images/our-story-01.jpg.webp" className='w-75 rounded' alt="" />
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <div>
+                                <h1 className='Greate-vibe f-primary'>Italian Restaurant</h1>
+                                <p className='f-gray fs-6'>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </div>
             </section>
 
@@ -67,6 +104,7 @@ function Home() {
                     <span className='fs-1 f-white poppins fw-semibold'>PATO PLACE</span>
                 </div>
             </section>
+
         </div>
     )
 }
